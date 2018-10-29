2f9626bba9b5c67af03f746df1084494665c552d
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-10-29 13:22:59 -0500

    Add files via upload

---
+++ Fourier.py
@@ -0,0 +1,24 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+#guardo los archivos
+datos1 = np.genfromtxt("signal.dat",delimiter=",")
+datos2 = np.genfromtxt("incompletos.dat", delimiter=",")
+
+#grafica sin modificar de signal.dat
+plt.plot(datos1[:,0],datos1[:,1])
+plt.savefig("MelendezLaura_signal.pdf")
+
+#mi funcion de fourier discreta modificada para un array y no una funcion
+
+def fourierdiscreta(f,n,k):
+    funcion = 0
+    f1 = f[:,1]
+    f2 = f[:,0]
+    for i in range(np.shape(f)[1]):         
+        funcion += f1[i]*(np.exp((-2j*np.pi*k*f2[i])/n))
+    return funcion
+
+print
+
+
