07af486c2707f509e14eccc9b6b4585b024398ca
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-10-31 03:18:35 -0500

    Add files via upload

--- Fourier.py
+++ Fourier.py
@@ -1,5 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
+import plotly.plotly as py
+import plotly.tools as tls
+from scipy.interpolate import interp1d
 from scipy.fftpack import fft, fftfreq
 
 #guardo los archivos
@@ -53,13 +56,84 @@ plt.savefig("MelendezLaura_TF.pdf")
 #con fft sin bonito 
 
 n = 512 #numero de datos que tenemos 
-dt = datos1[:,0]*n #numero de datos por unidad de frecuenca
+dt = 2*np.pi/(0.0272321428571-0.0271763392857) #numero de datos por unidad de frecuenca
 fft_x = fft(datos1[:,1]) / n # FFT Normalized
 freq = fftfreq(n, dt) # Recuperamos las frecuencias
 plt.figure()
 plt.plot(freq,abs(fft_x))
-plt.xscale('log')
 plt.show()
+
+#las hallé con la grafica que me dio, los picos.
+print("las frecuencias principales de mi señal son: -1.855e-07,-6.7649e-08,8.96178e-08,2.07567e-07 ")
+
+
+#Filtro pasabajas
+
+freq_cut = 1000
+fft_x[abs(freq) > freq_cut] = 0
+
+#con la inversa de fft 
+
+plt.figure()
+t = np.linspace(n-512,n,n) 
+clean_f = np.fft.ifft(fft_x) 
+
+plt.plot(t,np.real(clean_f), linewidth=5, color='green')
+plt.show()
+
+
+#LAURA ACUERDESE QUE LE FALTA EXPLICAR POR QUÉ LOS DATOS ESOS DE MERGA NO DAN 
+
+
+#Interpolacion de mis datos incorrectos 
+
+def interpolar (x,y):
+    xnuevo = np.linspace(0.000390625,0.028515625,512)
+    #flin = interp1d(x, y)
+    fcua = interp1d(x, y,kind ='quadratic')
+    fcub = interp1d(x, y,kind ='cubic')
+    
+    return xnuevo, fcua , fcub
+
+x,y1,y2 = interpolar(datos2[:,0],datos2[:,1])
+
+na = 512 #numero de datos que tenemos 
+dta = 2*np.pi/(0.0205915178571-0.0205357142857) #numero de datos por unidad de frecuenca
+fft_xa = fft(y1) / na # FFT Normalized
+freqa = fftfreq(na, dta) # Recuperamos las frecuencias
+plt.figure()
+
+
+
+
+
+fft_xb = fft(y2) / n # FFT Normalized
+freqb = fftfreq(na, dta) # Recuperamos las frecuencias
+  
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(221)
+ax1.plot(freqa,abs(fft_xa))
+
+ax2 = fig.add_subplot(222)
+ax2.plot(freqb,abs(fft_xb))
+
+ax3 = fig.add_subplot(223)
+ax3.plot(datos1[:,0],reales(fourierdiscreta(datos1)))
+
+plt.tight_layout()
+fig = plt.gcf()
+
+plotly_fig = tls.mpl_to_plotly( fig )
+plotly_fig['layout']['title'] = 'Transformadas de fourier '
+plotly_fig['layout']['margin'].update({'t':40})
+
+py.iplot(plotly_fig)
+plt.show()   
+
+
+
 
 
 
