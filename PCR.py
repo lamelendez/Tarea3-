32cf52313cfb1c24f6eede13b7b42326323ccf7b
lamelendez <42391440+lamelendez@users.noreply.github.com>
2018-10-30 20:24:46 -0500

    Add files via upload

--- PCR.py
+++ PCR.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 #extraer los datos
 
 data = (np.genfromtxt('WDBC.dat', delimiter = ','))[:,1:]
+data1 = np.delete(data,0,1)
 mb = (np.genfromtxt('WDBC.dat', delimiter = ',',dtype=str))[:,1]
 mb1 = np.array([0 if e == "M" else 1 for e in mb]) #cambiar los strings por numeros
 data[:,0]=mb1
@@ -20,7 +21,6 @@ def matrizcov(datax):
     for i in range(len(datax)):
         datax[i] = (datax[i]-np.mean(datax[i]))/np.std(datax[i]) #normalizada
 
-
     n_dim = np.shape(datax)[1]  
     m_dim = len(datax[0])  
     covarianza = np.ones([n_dim,n_dim])
@@ -32,22 +32,23 @@ def matrizcov(datax):
             #d = np.sqrt(varianza)
             covarianza[i,j] = np.sum((datax[:,i]-meani)*(datax[:,j]-meanj))/(m_dim-1)
     return covarianza 
-          
-#print(matrizcov(data))
+print(matrizcov(data1)[0])
 
-A = matrizcov(data)
+A = matrizcov(data1)
 
 val,vec = np.linalg.eig(A)
 
-print(val)
+#print(val)
 
 #calcular los autovectores de sus autovalores
 
-for i in range(np.shape(val)[0]):
-    print("el vector", vec[:,i], "corresponde al valor" , val[i])
+#for i in range(np.shape(val)[0]):
+ #   print("el vector", vec[:,i], "corresponde al valor" , val[i])
     
 
-#hallaremos los parametros más importantes en medida de sus porcentajes
+#hallaremos los parametros más importantes con los porcentajes            
+
+
 
 def porcentajemayores(array):  
     total = np.sum(array)
@@ -63,18 +64,19 @@ def porcentajemayores(array):
 
     return "los porcentajes de los mayores componentes son:", narray , "los componentes más importantes son los numeros:" , auto 
 
-print (porcentajemayores(val)) 
+#print (porcentajemayores(val)) 
      
- #las componentes más importantes son las dos primeras-
+#las componentes más importantes son las dos primeras-
 
-
-Pr= np.transpose(np.array([vec[0],vec[1]]))
-T = np.dot(data,Pr) #Proyeccion 
+#print(np.transpose(data1))
+Pr= (np.array([vec[0],vec[1]]))
+T = np.dot(Pr,np.transpose(data1)) #Proyeccion 
 
 #guardar las imagenes de las proyecciones 
 
-plt.scatter(T[:,0],T[:,1])
+plt.scatter(T[0,:],T[1,:])
 plt.savefig("MelendezLaura_PCA.pdf")
+plt.show()
 
 print( "Se muestra como PCA puede ayudar a la identificación de patrones que facilitarán posibles agrupamientos posteriores de tumores que podrían ser tediosos de otras formas" )
 
